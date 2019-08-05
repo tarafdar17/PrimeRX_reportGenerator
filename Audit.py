41ce13c1f0f8d3c1ac52cfc6a885411f6243c7d1
tarafdar17 <50920111+tarafdar17@users.noreply.github.com>
2019-08-05 18:21:24 -0400

    Update Audit.py

--- Audit.py
+++ Audit.py
@@ -1,87 +1,98 @@
 import pandas as pd
-from openpyxl import load_workbook
-import time 
+import time
+import os
+import sys
 
 
-def loadscript(step, trial, duration):
+def loadscript(a,b,c):
+    print(a,b,c)
+
+def loadscripts(step, trial, duration):
     print(step, end='', flush=True)
     for i in range(trial): 
         time.sleep(duration)
         print('.', end='', flush=True)
     time.sleep(.2)
     print("\n")
-    
-#writer = pd.ExcelWriter('TrialReport.xlsx')
 
-loadscript('Copying over PrimeRX', 6, .2)
-df = pd.read_csv('PrimeRx.csv', low_memory=False)
-#df = pd.read_csv('PrimeRx2.csv', low_memory=False)
-df0 = df[['NDC','DRGNAME','DRUGSTRONG', 'PACKAGESIZE','QUANT']]
-print(df.head())
-
-loadscript('Copying over Kinray RX', 6, .1)
-df1 = pd.read_excel('KINRX.xls',header= None, index= False , names=[0,1,2,3])
-df1 = df1.drop(df1.index[[0,1,2,3,4,5,6]])
-df1.columns = df1.iloc[0]
-df1 = df1.rename(columns={'Universal NDC': 'U NDC'})
-df1['NDC']=df1['U NDC'].astype(str).str[:5]+'-'+df1['U NDC'].astype(str).str[5:9]+'-'+df1['U NDC'].astype(str).str[-2:]
-df1 = df1.drop(df1.index[0])
-
-df1a = df1.groupby(['NDC'], as_index=False).sum()
-df1b = df1a[['NDC', 'Drug Name', 'Qty']]
-df1b = df1b.rename(columns={'Qty': 'KIN RX'})
-print(df1b.head())
-df1c = df1b[['NDC','KIN RX']]
+#loadscript('Copying over PrimeRX', 6, .4)
+writer = pd.ExcelWriter('TrialReport.xlsx', engine='xlsxwriter')
+rxRawDF = pd.read_csv('PrimeRx.csv', low_memory=False)
+rxRawDF.to_excel(writer, sheet_name='PrimeRx RAW', index=False)
+reportDF = rxRawDF[['NDC','DRGNAME','DRUGSTRONG', 'PACKAGESIZE','QUANT']]
+#print(rxRawDF.head(10))
 
 
-loadscript('Copying over Kinray OTC', 6, .1)
-df2 = pd.read_excel('KINOTC.xls',header= None, index= False , names=[0,1,2,3])
-df2 = df2.drop(df2.index[[0,1,2,3,4,5,6]])
-df2.columns = df2.iloc[0]
-df2 = df2.rename(columns={'Universal NDC': 'U NDC'})
-df2['NDC']=df2['U NDC'].astype(str).str[:5]+'-'+df2['U NDC'].astype(str).str[5:9]+'-'+df2['U NDC'].astype(str).str[-2:]
-df2 = df2.drop(df2.index[0])
-
-df2a = df2.groupby(['NDC'], as_index=False).sum()
-df2b = df2a[['NDC', 'Drug Name', 'Qty']]
-df2b = df2b.rename(columns={'Qty': 'KIN OTC'})
-print(df2b.head())
-df2c = df2b[['NDC','KIN OTC']]
-
-
-loadscript('Copying over McKesson', 6, .1)
-df3 = pd.read_csv('MCK.csv', header= None)
-df3 = df3.drop(df3.index[[0,1,2,3,4,5,6]])
-df3.columns = df3.iloc[0]
-df3 = df3.drop(df3.index[0])
-df3['NDC']=df3['NDC/UPC'].astype(str).str[:5]+'-'+df3['NDC/UPC'].astype(str).str[5:9]+'-'+df3['NDC/UPC'].astype(str).str[-2:]
-
-df3a = df3[['NDC', 'Item Description', 'Net']]
-df3b = df3a.groupby(['NDC', 'Item Description'], as_index=False).sum()
-df3b = df3b.rename(columns={'Net': 'MCK'})
-print(df3b.head())
-df3c = df3b[['NDC','MCK']]
+#loadscript('Copying over Kinray RX', 6, .4)
+if os.path.exists('KINRX.xls'):
+    kinrayRXDF = pd.read_excel('KINRX.xls',header= None, index= False , names=[0,1,2,3])
+    kinrayRXDF = kinrayRXDF.drop(kinrayRXDF.index[[0,1,2,3,4,5,6]])
+    kinrayRXDF.columns = kinrayRXDF.iloc[0]
+    kinrayRXDF = kinrayRXDF.rename(columns={'Universal NDC': 'U NDC'})
+    kinrayRXDF['NDC']=kinrayRXDF['U NDC'].astype(str).str[:5]+'-'+kinrayRXDF['U NDC'].astype(str).str[5:9]+'-'+kinrayRXDF['U NDC'].astype(str).str[-2:]
+    kinrayRXDF = kinrayRXDF.drop(kinrayRXDF.index[0])
+    kinrayRXDF = kinrayRXDF.groupby(['NDC'], as_index=False).sum()
+    kinrayRXDF = kinrayRXDF[['NDC', 'Drug Name', 'Qty']]
+    kinrayRX_sheet = kinrayRXDF[['NDC', 'Drug Name', 'Qty']]
+    kinrayRXDF = kinrayRXDF.rename(columns={'Qty': 'KIN RX'})
+    #print(kinrayRXDF.head())
+    kinrayRXDF = kinrayRXDF[['NDC','KIN RX']]
+    kinrayRX_sheet.to_excel(writer, sheet_name='KIN_RX', index=False)
+else:
+    kinrayRXDF = pd.DataFrame(columns=['NDC']) 
 
 
-loadscript('Creating Report sheet', 6, .4)
-df0 = df.groupby(['NDC','DRGNAME','DRUGSTRONG', 'PACKAGESIZE'], as_index=False).sum()
-print(df0.head())
+#loadscript('Copying over Kinray OTC', 6, .3)
+if os.path.exists('KINOTC.xls'): 
+    kinrayOTCDF = pd.read_excel('KINOTC.xls',header= None, index= False , names=[0,1,2,3])
+    kinrayOTCDF = kinrayOTCDF.drop(kinrayOTCDF.index[[0,1,2,3,4,5,6]])
+    kinrayOTCDF.columns = kinrayOTCDF.iloc[0]
+    kinrayOTCDF = kinrayOTCDF.rename(columns={'Universal NDC': 'U NDC'})
+    kinrayOTCDF['NDC']=kinrayOTCDF['U NDC'].astype(str).str[:5]+'-'+kinrayOTCDF['U NDC'].astype(str).str[5:9]+'-'+kinrayOTCDF['U NDC'].astype(str).str[-2:]
+    kinrayOTCDF = kinrayOTCDF.drop(kinrayOTCDF.index[0])
+    kinrayOTCDF = kinrayOTCDF.groupby(['NDC'], as_index=False).sum()
+    kinrayOTCDF = kinrayOTCDF[['NDC', 'Drug Name', 'Qty']]
+    kinrayOTC_sheet = kinrayOTCDF[['NDC', 'Drug Name', 'Qty']]
+    kinrayOTCDF = kinrayOTCDF.rename(columns={'Qty': 'KIN OTC'})
+    #print(kinrayOTCDF.head())
+    kinrayOTCDF = kinrayOTCDF[['NDC','KIN OTC']]
+    kinrayOTC_sheet.to_excel(writer, sheet_name='KIN_OTC',index=False)
+else:
+    kinrayOTCDF = pd.DataFrame(columns=['NDC']) 
 
-df0a = pd.merge(df0, df1c, on=['NDC'])
-print(df0a.head())
 
 
-loadscript('Setting up Excel Writing using Pandas DataFrames & OpenPYxl', 6, .2)
-fn = 'TrialReport.xlsx'
-writer = pd.ExcelWriter(fn, engine='openpyxl')
-book = load_workbook(fn)
-writer.book = book
-writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+#loadscript('Copying over McKesson', 6, .3)
+if os.path.exists('MCK.csv'): 
+    MCKDF = pd.read_csv('MCK.csv', header= None)
+    MCKDF = MCKDF.drop(MCKDF.index[[0,1,2,3,4,5,6]])
+    MCKDF.columns = MCKDF.iloc[0]
+    MCKDF = MCKDF.drop(MCKDF.index[0])
+    MCKDF['NDC']=MCKDF['NDC/UPC'].astype(str).str[:5]+'-'+MCKDF['NDC/UPC'].astype(str).str[5:9]+'-'+MCKDF['NDC/UPC'].astype(str).str[-2:]
+    MCKDF['Net']=MCKDF['Net'].apply(float)
+    MCK_sheet = MCKDF[['NDC','Item Description', 'Net']]
+    #print(MCKDF.head())
+    MCKDF = MCKDF.rename(columns={'Net': 'MCK'})
+    MCKDF = MCKDF[['NDC','Item Description', 'MCK']]
+    MCKDF = MCKDF[['NDC','MCK']]
+    MCK_sheet.to_excel(writer, sheet_name='MCK', index=False)
+else:
+    kinrayRXDF = pd.DataFrame(columns=['NDC']) 
 
-loadscript('Exporting to Excel Workbook: TrialReport.xlsx', 6, .6)
-df.to_excel(writer, sheet_name='PrimeRxRAW', index=False)
-df0a.to_excel(writer, sheet_name='PrimeRx', index=False)
-df1.to_excel(writer, sheet_name='KIN_RX', index=False)
-df2.to_excel(writer, sheet_name='KIN_OTC',index=False)
-df3.to_excel(writer, sheet_name='MCK', index=False)
+
+#loadscript('Creating Report sheet', 6, .6)
+reportDF = reportDF.groupby(['NDC','DRGNAME','DRUGSTRONG', 'PACKAGESIZE'], as_index=False).sum()
+reportDF['Total Dispense'] = reportDF['QUANT']/reportDF['PACKAGESIZE']
+reportDF['Total Dispense'] = reportDF['Total Dispense'].apply(lambda x:round(x,1))
+reportDF1 = pd.merge(reportDF, kinrayRXDF, on=['NDC'], how='left')
+reportDF2 = pd.merge(reportDF1, kinrayOTCDF, on=['NDC'], how='left')
+reportDF = pd.merge(reportDF2, MCKDF, on=['NDC'], how='left')
+reportDF['Total Purchase'] = reportDF['KIN RX'] + reportDF['MCK']
+reportDF['DISP'] = reportDF['Total Dispense'] - reportDF['Total Purchase']
+reportDF.to_excel(writer, sheet_name='Report', index=False)
+
 writer.save()
+os.system("open -a 'Microsoft Excel.app' 'TrialReport.xlsx'")
+# Windows - os.system('start excel.exe "%s\\TrialReport.xls"' % (sys.path[0], ))
+
+
